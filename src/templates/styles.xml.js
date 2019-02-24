19d3105a691e352ff45d2c5654942e4e90eb3f97
Constantine kuks <conkuk@gmail.com>
2019-02-24 13:42:51 +0000

    More xml fixes

--- src/templates/styles.xml.js
+++ src/templates/styles.xml.js
@@ -1,2 +1,2 @@
 export default `<?xml version="1.0" encoding="utf-8" standalone="yes"?>
-<styleSheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">{placeholder}</styleSheet>`;
+<styleSheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:x14ac="http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac">{placeholder}</styleSheet>`;
--- src/templates/styles_inner_template.xml.js
+++ src/templates/styles_inner_template.xml.js
@@ -1,14 +1,21 @@
-export default `<fonts count="1">
+export default `
 <fonts count="1" x14ac:knownFonts="1">
-        <font>
-            <sz val="11"/>
-            <color theme="1"/>
-            <name val="Calibri"/>
-            <family val="2"/>
-            <scheme val="minor"/>
-        </font>
-    </fonts>
-{fillsPlaceholder}
+    <font>
+        <sz val="11"/>
+        <color theme="1"/>
+        <name val="Calibri"/>
+        <family val="2"/>
+        <scheme val="minor"/>
+    </font>
+</fonts>
+<fills count="2">
+    <fill>
+        <patternFill patternType="none"/>
+    </fill>
+    <fill>
+        <patternFill patternType="gray125"/>
+    </fill>
+</fills>
 <borders count="1">
     <border>
         <left/>
@@ -29,4 +36,16 @@ export default `<fonts count="1">
 </cellXfs>
 <cellStyles count="1">
     <cellStyle name="Normal" xfId="0" builtinId="0"/>
-</cellStyles>`;
+</cellStyles>
+<dxfs count="0"/>
+<tableStyles count="0" defaultTableStyle="TableStyleMedium2" defaultPivotStyle="PivotStyleLight16"/>
+<extLst>
+    <ext uri="{EB79DEF2-80B8-43e5-95BD-54CBDDF9020C}"
+         xmlns:x14="http://schemas.microsoft.com/office/spreadsheetml/2009/9/main">
+        <x14:slicerStyles defaultSlicerStyle="SlicerStyleLight1"/>
+    </ext>
+    <ext uri="{9260A510-F301-46a8-8635-F512D64BE5F5}"
+         xmlns:x15="http://schemas.microsoft.com/office/spreadsheetml/2010/11/main">
+        <x15:timelineStyles defaultTimelineStyle="TimeSlicerStyleLight1"/>
+    </ext>
+</extLst>`;
